f216fcd662d6aca038833e8b63384be3327e395a
dietcoke <34706050+dietcoke@users.noreply.github.com>
2018-05-14 18:16:15 -0700

    Update ImplementQueuewithLimitedSizeofArrays.cpp

--- abb/ImplementQueuewithLimitedSizeofArrays.cpp
+++ abb/ImplementQueuewithLimitedSizeofArrays.cpp
@@ -3,80 +3,80 @@ typedef list<vector<int>>::iterator LITER;
 
 class QueueWithFixedArray
 {
-  public:
-    QueueWithFixedArray(int fixSize)
-    {
-        if (fixSize < 1)
-        {
-            //;
-        }
+public:
+	QueueWithFixedArray(int fixSize)
+	{
+		if (fixSize < 1)
+		{
+			//;
+		}
 
-        this->fixSize = fixSize;
-        init();
-    }
+		this->fixSize = fixSize;
+		init();
+	}
 
-    void init()
-    {
-        q.push_back(vector<int>(fixSize));
-        sLITER = q.begin();
-        eLITER = q.begin();
+	void init()
+	{
+		q.push_back(vector<int>(fixSize));
+		sLITER = q.begin();
+		eLITER = q.begin();
 
-        start = 0;
-        end = 0;
-        count = 0;
-    }
+		start = 0;
+		end = 0;
+		count = 0;
+	}
 
-    void enqueue(int num)
-    {
-        //process full
+	void enqueue(int num)
+	{
+		//process full
+		(*eLITER)[end++] = num;
 
-        if (end == fixSize - 1)
-        {
-            q.push_back(vector<int>(fixSize));
-            eLITER = --q.end();
-            end = 0;
-        }
+		if (end == fixSize)
+		{
+			q.push_back(vector<int>(fixSize));
+			eLITER = --q.end();
+			end = 0;
+		}
 
-        (*eLITER)[end++] = num;
-        count++;
-    }
+		count++;
+	}
 
-    int deque()
-    {
-        if (count == 0)
-        {
-            //throw
-            return 1;
-        }
+	int deque()
+	{
+		if (count == 0)
+		{
+			//throw
+			return 1;
+		}
 
-        int result = (*sLITER)[start++];
-        count--;
-        if (start == fixSize)
-        {
-            q.pop_front();
-            if (q.size() == 0)
-            {
-                init();
-            }
-            else
-            {
-                start = 0;
-                sLITER = q.begin();
-            }
-        }
+		int result = (*sLITER)[start++];
+		count--;
+		if (start == fixSize)
+		{
+			q.pop_front();
+			if (q.size() == 0)
+			{
+				init();
+			}
+			else
+			{
+				start = 0;
+				sLITER = q.begin();
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-  private:
-    int fixSize;
-    int count;
+private:
+	int fixSize;
+	int count;
 
-    list<vector<int>> q;
+	list<vector<int>> q;
 
-    LITER sLITER;
-    LITER eLITER;
+	LITER sLITER;
+	LITER eLITER;
 
-    int start;
-    int end;
+	int start;
+	int end;
 };
