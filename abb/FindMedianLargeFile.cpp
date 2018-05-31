98b6f3e8853264392af9b416a575a313bb648583
dietcoke <34706050+dietcoke@users.noreply.github.com>
2018-05-31 14:03:05 -0700

    Update FindMedianLargeFile.cpp

--- abb/FindMedianLargeFile.cpp
+++ abb/FindMedianLargeFile.cpp
@@ -1,61 +1,59 @@
-package find_median_in_large_file_of_integers;
-
-import org.junit.*;
-
-import static org.junit.Assert.*;
-
-public class FindMedianinLargeIntegerFileofIntegers {
-    /*
-        Find Median in Large Integer File of Integers
-        AirBnB Interview Question
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers
+     * @return: An integer denotes the middle number of the array
      */
-    public class Solution {
-        private long search(int[] nums, int k, long left, long right) {
-            if (left >= right) {
-                return left;
-            }
-
-            long res = left;
-            long guess = left + (right - left) / 2;
-            int count = 0;
-            for (int num : nums) {
-                if (num <= guess) {
-                    count++;
-                    res = Math.max(res, num);
-                }
-            }
-
-            if (count == k) {
-                return res;
-            } else if (count < k) {
-                return search(nums, k, Math.max(res + 1, guess), right);
-            } else {
-                return search(nums, k, left, res);
-            }
+    int median(vector<int> &nums) {
+        // write your code here
+        if(nums.size()==0)
+        {
+            return -1;
         }
-
-        public double findMedian(int[] nums) {
-            int len = 0;
-            for (int num : nums) {
-                len++;
-            }
-
-            if (len % 2 == 1) {
-                return (double) search(nums, len / 2 + 1, Integer.MIN_VALUE, Integer.MAX_VALUE);
-            } else {
-                return (double) (search(nums, len / 2, Integer.MIN_VALUE, Integer.MAX_VALUE) +
-                        search(nums, len / 2 + 1, Integer.MIN_VALUE, Integer.MAX_VALUE)) / 2;
-            }
+        
+        if(nums.size()==1)
+        {
+            return nums[0];
         }
+        
+        int n = nums.size();
+        int t = (n-1)/2;
+        //less equal count = t+1
+        
+        return findMedian(nums, t+1, INT_MIN, INT_MAX);
     }
-
-    public static class UnitTest {
-        @Test
-        public void test1() {
-            Solution sol = new FindMedianinLargeIntegerFileofIntegers().new Solution();
-            assertEquals(3.0, sol.findMedian(new int[]{3, -2, 7}), 1E-03);
-            assertEquals(5.0, sol.findMedian(new int[]{-100, 99, 3, 0, 5, 7, 11, 66, -33}), 1E-03);
-            assertEquals(4.5, sol.findMedian(new int[]{4, -100, 99, 3, 0, 5, 7, 11, 66, -33}), 1E-03);
+    
+    int findMedian(vector<int> &nums, int count, long long left, long long right)
+    {
+        if(left==right)
+        {
+            return left;
         }
+        
+        long long guess = left + (right - left)/2;
+        
+        int maxNum = left;
+        int actualCount = 0;
+        for(auto num : nums)
+        {
+            if(num <= guess)
+            {
+                maxNum = max(maxNum, num);
+                actualCount++;
+            }
+        }
+        
+        if(actualCount == count)
+        {
+            return maxNum;
+        }
+        
+        if(actualCount < count)
+        {
+            return findMedian(nums, count,guess+1, right);
+        }
+        
+        return findMedian(nums, count, left, maxNum);
     }
-}
+    
+};
